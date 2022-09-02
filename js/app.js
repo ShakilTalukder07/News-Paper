8a0cc3ebb6cbb16b93b237a2421d6ad14cd63919
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-09-03 02:23:10 +0600

    question section complete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-const loadAllProducts = async() =>{
-    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+const loadAllProducts = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
     const data = await response.json();
+    // console.log(data);
     return data;
-}
+};
 
-const setAllMenu = async () =>{
-    console.log(loadAllProducts());
-}
+const setAllMenu = async () => {
+    const data = await loadAllProducts();
+    console.log(data.data);
+    const menu = document.getElementById('menu');
+
+    for (const status in data.data.news_category.category_name) 
+    {
+        // console.log(status.category_name);
+            const li = document.createElement("li");
+            ul.innerHTML = `<a>${status.category_name}</a>`;
+            menu.appendChild(li);
+    }
+
+};
+
+setAllMenu()
